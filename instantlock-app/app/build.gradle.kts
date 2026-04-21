e9ab60b657ac13ca302aee555e7b3d9213c8aadb
wiraSan1262 <>
2026-04-21 08:59:50 +0700

    bump up

--- instantlock-app/app/build.gradle.kts
+++ instantlock-app/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.risyan.quickshutdownphone"
         minSdk = 30  // Android 11 (R) - Required for AccessibilityService.takeScreenshot()
         targetSdk = 35
-        versionCode = 16
-        versionName = "0.11.0"
+        versionCode = 17
+        versionName = "0.12.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
